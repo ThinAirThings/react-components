a64105f77ddc17088fbe23ee59f98134cebb2e94
irishcarbomb777 <danielclannan@gmail.com>
2023-04-28 10:35:06 -0400

    mod

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,14 @@
-export {Button} from './Button/Button';
-export * from './LoadingSpinner/LoadingSpinner';
-export * from './Stacks/HStack';
-export * from './Stacks/VStack';
+
+import {Button} from './Button/Button';
+import {LoadingSpinner} from './LoadingSpinner/LoadingSpinner';
+import {HStack} from './Stacks/HStack';
+import {VStack} from './Stacks/VStack';
+import { TextInput } from './TextInput/TextInput';
+
+export {
+    Button,
+    LoadingSpinner,
+    HStack,
+    VStack,
+    TextInput
+}
